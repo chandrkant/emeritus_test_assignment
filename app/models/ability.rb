1b33f34a3be2326f73084e3f42aa2a395e990a60
Chandrakant Solanki <chandrakant.solanki@reddoorz.com>
2023-10-05 17:00:44 +0530

    UI changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,13 +19,13 @@ class Ability
     elsif user.schooladmin?
       can %i[update show], School, user_id: user.id
       can :manage, Course
-      can :manage, Batch, school_id: user.school.id
+      can :manage, Batch, school_id: user.school&.id
       can %i[update index], Enrollment, batch: { school: { user_id: user.id } }
     elsif user.student?
-      can :read, School
+      can %i[index show], School
       can :read, Course
       can %i[create index], Enrollment, user_id: user.id
-      can :show_class_mates, Enrollment
+      can %i[show_class_mates], Enrollment
       can :read, Batch
     end
   end
